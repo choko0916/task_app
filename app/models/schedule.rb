a255f2f3321b9f877da124eb9fb03bf0dda418b0
choko0916 <n.yuka0702@gmail.com>
2022-04-05 03:36:44 +0900

    終了日が開始日の後になるようにバリデーションを追加、css追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,12 @@
 class Schedule < ApplicationRecord
   validates :title, :start_date, :end_date, presence: true
-  @date = Date.today.strftime("%Y年%m月%d日")
-  
+  validate :end_date_cannot_be_before_start_date
+
+  def end_date_cannot_be_before_start_date
+    if start_date > end_date
+      errors.add(:end_date, "は開始日以降の日付を選択してください") 
+    end
+  end
+
 end
+  
